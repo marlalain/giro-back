22db0082c214ce621a3587adc909738d34fd4bff
Paulo Elienay <paulo@pauloelienay.com>
2021-10-31 13:24:39 -0300

    chore(start.spring): inits project

---
+++ src/main/java/com/pauloelienay/giroback/GiroBackApplication.java
@@ -0,0 +1,13 @@
+package com.pauloelienay.giroback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GiroBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GiroBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pauloelienay/giroback/GiroBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pauloelienay.giroback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GiroBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
